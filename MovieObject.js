0572bc4fcece15aba3e2b83467e61527e3498c5f
Nisan Ercan Tarhan <nisan_tarhan@hotmail.com>
2021-02-07 15:31:07 +0300

    Created file which names MovieObject for Challenge 2

---
+++ MovieObject.js
@@ -0,0 +1,16 @@
+// Movie object that takes in five arguments
+
+// Title
+// Director
+// Genre
+// Release year
+// Rating
+
+/*
+The movie prototype should have a function called getOverview,
+which logs the following overview for each film:
+
+"<movie>, a <genre> film directed by <director> was released in
+<releaseYear>. It received a rating of <rating>."
+*/
+
